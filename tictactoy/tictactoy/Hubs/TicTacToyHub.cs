9521c68244ce6826e0508dd8509e0c614914c6f3
Joarley Santos <joarley.santos@gmail.com>
2015-09-01 21:50:33 -0300

    _

---
+++ tictactoy/tictactoy/Hubs/TicTacToyHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tictactoy.Hubs
+{
+    public class TicTacToyHub : Hub
+    {
+        Dictionary<int, object> jogadas = new Dictionary<int, object>();
+
+        public void Jogar(int pos)
+        {
+            
+
+
+        }
+    }
+}
